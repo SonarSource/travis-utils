b136252d7af2b5e8320622ac59201b50cb6ff273
David Gageot <david@gageot.net>
2015-06-03 09:50:59 +0200

    misc

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ TRAVIS_UTILS_HOME=/tmp/travis-utils
 mkdir -p $TRAVIS_UTILS_HOME
 curl -sSL https://github.com/dgageot/travis-utils/tarball/master | tar zx --strip-components 1 -C $TRAVIS_UTILS_HOME
 
-source $TRAVIS_UTILS_HOME/utils.sh
+cd $TRAVIS_UTILS_HOME
+source utils.sh
