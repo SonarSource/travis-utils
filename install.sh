9c644ff8d49637035fb4605b9e37bcec22005683
David Gageot <david@gageot.net>
2015-06-03 10:35:52 +0200

    Sourcing should be done outside

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
 TRAVIS_UTILS_HOME=/tmp/travis-utils
 mkdir -p $TRAVIS_UTILS_HOME
 curl -sSL https://github.com/dgageot/travis-utils/tarball/master | tar zx --strip-components 1 -C $TRAVIS_UTILS_HOME
-
-cd $TRAVIS_UTILS_HOME
-source utils.sh
