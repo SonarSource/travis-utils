2d8f0dfaa8db3a6f383f08358a94e7cd4ffa2138
David Gageot <david@gageot.net>
2015-06-03 09:49:37 +0200

    Shorter syntax

--- utils.sh
+++ utils.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-function reset_ruby() {
+function reset_ruby {
 	unset GEM_PATH GEM_HOME RAILS_ENV
 }
 
-function install_jars() {
+function install_jars {
   echo "Install jars into local maven repository"
 
   mkdir -p ~/.m2/repository
@@ -12,13 +12,13 @@ function install_jars() {
 }
 
 # Usage: fetch "directory" "user/project" "branch"
-function fetch() {
+function fetch {
   mkdir -p $1
   curl -su dgageot:$ITS_TOKEN -L https://github.com/$2/tarball/$3 | tar zx --strip-components 1 -C $1
 }
 
 # Usage: build "directory" "user/project" "branch" "build command" ["FORCE"]
-function build() {
+function build {
   SHA1=$(curl -su dgageot:$ITS_TOKEN -L https://api.github.com/repos/$2/git/refs/heads/$3 | jq -r .object.sha)
 
   if [ -f "$HOME/.m2/repository/$SHA1" ]; then
@@ -39,27 +39,27 @@ function build() {
 }
 
 # Usage: build_sonarqube "BRANCH"
-function build_sonarqube() {
+function build_sonarqube {
   build "/tmp/sonarqube_$1" "SonarSource/sonarqube" "$1" "mvn install -DskipTests -Pdev"
 }
 
 # Usage: build_parent_pom "VERSION"
-function build_parent_pom() {
+function build_parent_pom {
   build "/tmp/parent_$1" "SonarSource/parent" "$1" "mvn install -DskipTests"
 }
 
 # Usage: build_orchestrator "VERSION"
-function build_orchestrator() {
+function build_orchestrator {
   build "/tmp/orchestrator_$1" "SonarSource/sonar-orchestrator" "$1" "mvn install -DskipTests"
 }
 
 # Usage: fetch_its
-function fetch_its() {
+function fetch_its {
   fetch "/tmp/its" "SonarSource/sonar-tests-languages" "master"
 }
 
 # Usage: create_orchestrator_properties
-function create_orchestrator_properties() {
+function create_orchestrator_properties {
   PROPERTIES=/tmp/orchestrator.properties
 
   echo "sonar.jdbc.dialect=embedded" > $PROPERTIES
@@ -70,7 +70,7 @@ function create_orchestrator_properties() {
 }
 
 # Usage: run_its "SONAR_VERSION"
-function run_its() {
+function run_its {
 	reset_ruby
 	install_jars
 
