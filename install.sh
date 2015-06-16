a010a8a5ba593a617306d12259733de398ff77d6
David Gageot <david@gageot.net>
2015-06-16 14:03:58 +0200

    prepare for v1

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 TRAVIS_UTILS_HOME=/tmp/travis-utils
 mkdir -p $TRAVIS_UTILS_HOME
-curl -sSL https://github.com/dgageot/travis-utils/tarball/master | tar zx --strip-components 1 -C $TRAVIS_UTILS_HOME
+curl -sSL https://github.com/dgageot/travis-utils/tarball/v1 | tar zx --strip-components 1 -C $TRAVIS_UTILS_HOME
