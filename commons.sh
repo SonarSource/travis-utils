605d58c3341924f21b222509fabc210f6829b990
David Gageot <david@gageot.net>
2015-06-24 15:24:52 +0200

    Trying to build with 2 threads

--- commons.sh
+++ commons.sh
@@ -78,7 +78,7 @@ function run_its {
   fi
 
   cd its/plugin
-  mvn -Dmaven.test.redirectTestOutputToFile=false -Dsonar.runtimeVersion="$VERSION" install
+  mvn -T2 -Dmaven.test.redirectTestOutputToFile=false -Dsonar.runtimeVersion="$VERSION" install
 
   unset VERSION
 }
