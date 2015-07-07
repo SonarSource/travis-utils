1bb81527d6dc8ab4f6774edd14b2e78c5e700e0c
David Gageot <david@gageot.net>
2015-07-07 18:26:47 +0200

    fix xvfb viewport

--- commons.sh
+++ commons.sh
@@ -92,7 +92,7 @@ function run_its {
 # Usage: start_xvfb
 function start_xvfb {
   export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start
+  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 }
 
 # Usage: sonarqube_its "category"
