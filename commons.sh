5c9f5ab1a498c8a17e6be18d4cb7fe3c39df79a2
David Gageot <david@gageot.net>
2015-07-09 18:20:25 +0200

    Fix ITs for sonar-java

--- commons.sh
+++ commons.sh
@@ -84,7 +84,7 @@ function run_plugin_its {
   fi
 
   cd "$1"
-  mvn -Dmaven.test.redirectTestOutputToFile=false -Dsonar.runtimeVersion="SONAR_VERSION" test $OPTIONS
+  mvn -Dmaven.test.redirectTestOutputToFile=false -Dsonar.runtimeVersion="SONAR_VERSION" install $OPTIONS
 
   unset VERSION
 }
