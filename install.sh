7914d4840ee97763bf5e71b8d1f347bee56ee7af
David Gageot <david@gageot.net>
2015-07-13 13:27:21 +0200

    Clean up code

--- install.sh
+++ install.sh
@@ -2,15 +2,9 @@
 
 set -euo pipefail
 
+# Copy all files either from local copy (testing) or from github
 TRAVIS_UTILS_HOME=/tmp/travis-utils
-
-function install_script() {
-  echo -e "#!/bin/bash\n\nset -euo pipefail\n\nsource $TRAVIS_UTILS_HOME/commons.sh\n\n$1 \$@" > ~/.local/bin/travis_$1
-  chmod u+x ~/.local/bin/travis_$1
-}
-
 mkdir -p $TRAVIS_UTILS_HOME
-
 if [ "${1:-}" == "LOCAL" ]; then
   cp -R . $TRAVIS_UTILS_HOME
 else
@@ -18,6 +12,11 @@ else
 fi
 
 # Create shortcuts that are in the PATH
+function install_script() {
+  echo -e "#!/bin/bash\n\nset -euo pipefail\n\nsource $TRAVIS_UTILS_HOME/commons.sh\n\n$1 \$@" > ~/.local/bin/travis_$1
+  chmod u+x ~/.local/bin/travis_$1
+}
+
 mkdir -p ~/.local/bin
 install_script "runDatabaseCI"
 install_script "sonarqube_its"
@@ -27,7 +26,6 @@ install_script "build"
 install_script "download_sonarqube_release"
 
 # Complete the installation
-
 echo "Install jars into local maven repository"
 mkdir -p ~/.m2/repository
 cp -r /tmp/travis-utils/m2repo/* ~/.m2/repository
