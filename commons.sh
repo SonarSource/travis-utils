4ae818b47c90323327fd4a942cee0371d219095f
David Gageot <david@gageot.net>
2015-07-13 14:25:39 +0200

    Make it easier to start xvfb

--- commons.sh
+++ commons.sh
@@ -55,7 +55,6 @@ function download_sonarqube_release {
 
 # Usage: start_xvfb
 function start_xvfb {
-  export DISPLAY=:99.0
   /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 }
 
--- env.sh
+++ env.sh
@@ -2,5 +2,6 @@
 
 export TRAVIS_UTILS_HOME=/tmp/travis-utils
 export ORCHESTRATOR_CONFIG_URL="file://$TRAVIS_UTILS_HOME/orchestrator.properties"
+export DISPLAY=:99.0
 
 unset GEM_PATH GEM_HOME RAILS_ENV
