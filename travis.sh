c0280ee5678f86c37290453c6bc794ede51382e0
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-11-05 21:55:41 +0100

    Fix execution of sibling scripts

--- travis.sh
+++ travis.sh
@@ -11,24 +11,50 @@ function installTravisTools {
   source ~/.local/bin/install
 }
 
+function assertFileContains {
+  FILE=$1
+  EXPECTED=$2
+  if ! grep -q "$EXPECTED" $FILE; then
+    echo "File $FILE does not contain $EXPECTED"
+    echo "Got:"
+    cat $FILE
+    exit 1
+  fi
+}
+
 installTravisTools
 
-case "$TEST" in
+echo "------- build_snapshot of public project"
+LOG_FILE=/tmp/build_public_snapshot.log
+build_snapshot "SonarSource/parent-oss" > $LOG_FILE
+assertFileContains $LOG_FILE "Get SHA1 of \[SonarSource/parent-oss:HEAD\]"
+assertFileContains $LOG_FILE "Build \[SonarSource/parent-oss:"
+assertFileContains $LOG_FILE "BUILD SUCCESS"
+# reuse cache
+build_snapshot "SonarSource/parent-oss" > $LOG_FILE
+assertFileContains $LOG_FILE "Get SHA1 of \[SonarSource/parent-oss:HEAD\]"
+assertFileContains $LOG_FILE "is already on cache"
 
-BUILD_PUBLIC_SNAPSHOT)
-  build_snapshot "SonarSource/parent-oss"
-  ;;
 
-BUILD_PRIVATE_SNAPSHOT)
-  build_snapshot "SonarSource/parent"
-  ;;
 
-BUILD_SHA1)
-  build "SonarSource/parent-oss" "24"
-  ;;
+echo "------- build_snapshot of private project"
+LOG_FILE=/tmp/build_private_snapshot.log
+build_snapshot "SonarSource/parent" > $LOG_FILE
+assertFileContains $LOG_FILE "Get SHA1 of \[SonarSource/parent:HEAD\]"
+assertFileContains $LOG_FILE "Build \[SonarSource/parent:"
+assertFileContains $LOG_FILE "BUILD SUCCESS"
+# reuse cache
+build_snapshot "SonarSource/parent" > $LOG_FILE
+assertFileContains $LOG_FILE "Get SHA1 of \[SonarSource/parent:HEAD\]"
+assertFileContains $LOG_FILE "is already on cache"
 
-*)
-  echo "Unexpected TEST value: $TEST"
-  exit 1
-  ;;
-esac
+
+
+echo "------- build sha1"
+LOG_FILE=/tmp/build_sha1.log
+build "SonarSource/parent-oss" "24" > $LOG_FILE
+assertFileContains $LOG_FILE "Build \[SonarSource/parent-oss:24\]"
+assertFileContains $LOG_FILE "BUILD SUCCESS"
+# reuse cache
+build "SonarSource/parent-oss" "24" > $LOG_FILE
+assertFileContains $LOG_FILE "is already on cache"
